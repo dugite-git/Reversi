e6c662a633dd361f0daf6f47a2299a953d65d665
dugite-git <tubasa283one@gmail.com>
2024-05-22 02:22:34 +0900

    完成（まじで）

--- Assets/Scripts/ReversiManager.cs
+++ Assets/Scripts/ReversiManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.Properties;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
